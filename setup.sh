987c39ba0cbadb4005244a4c40c971d38bfdbe3c
szaporta <sarah.zaporta@datadoghq.com>
2017-12-17 12:16:08 -0500

    Create setup.sh
    This sets up the lamp stack, creates the sakila database to store the sample schema/data in, and creates the php info file to test out the php installation

---
+++ setup.sh
@@ -0,0 +1,23 @@
+# This sets up a LAMP stack for you in Ubuntu (I used Ubuntu 16.04)
+
+# Install Apache
+apt-get install apache2 -y
+
+# Install PHP
+apt-get install php 7.0 php7.0-cli libapache2-mod-php7.0 php-mcrypt php-mysql -y
+
+# Install MySQL
+apt-get install mysql-client mysql-server -y
+mysql_secure_installation #be sure to harden your mysql security
+
+# Navigate to the directory with the sample databases in it, for automated loading
+cd /sakila-db/
+
+# Upload the sample databases to the database
+mysql -u root -plimber  --execute="CREATE DATABASE Sakila; SOURCE sakila-schema.sql; SOURCE sakila-data.sql;"
+
+# Navigate to web directory for LAMP stack
+cd /var/www/html
+
+# Create PHP file to test
+echo '<?php phpinfo(); ?>' > info.php
