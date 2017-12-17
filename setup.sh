0bb5569e53207d7b7d8bf898fc3b7c1ec168e83b
szaporta <sarah.zaporta@datadoghq.com>
2017-12-17 13:05:55 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,28 @@
-# This sets up a LAMP stack for you in Ubuntu (I used Ubuntu 16.04)
+# This sets up a LAMP stack for you in Ubuntu (I used Ubuntu Xenial - 16.04)
 
 # Install Apache
 apt-get install apache2 -y
 
 # Install PHP
-apt-get install php 7.0 php7.0-cli libapache2-mod-php7.0 php-mcrypt php-mysql -y
+apt-get install php7.0 php7.0-cli libapache2-mod-php7.0 php-mcrypt php-mysql -y
 
 # Install MySQL
 apt-get install mysql-client mysql-server -y
-mysql_secure_installation #be sure to harden your mysql security
+
+# Harden MySQL security
+mysql_secure_installation
 
 # Navigate to the directory with the sample databases in it, for automated loading
-cd /sakila-db/
+cd sakila-db
 
-# Upload the sample databases to the database
-mysql -u root -plimber  --execute="CREATE DATABASE Sakila; USE Sakila; SOURCE sakila-schema.sql; SOURCE sakila-data.sql;"
+# Create the Sakila database
+mysql -uroot -plimber  --execute="CREATE DATABASE Sakila;"
+
+# Upload the schema for the Sakila database
+mysql -uroot -plimber Sakila < sakila-schema.sql
+
+# Upload the data for the Sakila database
+mysql -uroot -plimber Sakila < sakila-data.sql
 
 # Navigate to web directory for LAMP stack
 cd /var/www/html
