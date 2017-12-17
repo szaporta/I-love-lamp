244855c3ba6a547b7943b23989fe100f0c026609
szaporta <sarah.zaporta@datadoghq.com>
2017-12-17 14:39:46 -0500

    Create agent_install.sh

---
+++ agent_install.sh
@@ -0,0 +1,2 @@
+# Change API KEY!
+DD_API_KEY=d5ed33bc1830f93767bbc1a16056ca95 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
