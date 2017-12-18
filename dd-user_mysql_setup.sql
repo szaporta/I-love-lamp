352cfbd86bd415643439a74417a6c3ba79250ed6
szaporta <sarah.zaporta@datadoghq.com>
2017-12-18 13:15:28 -0500

    Update dd-user_mysql_setup.sql

--- dd-user_mysql_setup.sql
+++ dd-user_mysql_setup.sql
@@ -1,4 +1,4 @@
 CREATE USER 'datadog'@'localhost' IDENTIFIED BY 'hKsJzswSeY=J88vSQixGdxb1';
 GRANT REPLICATION CLIENT ON *.* TO 'datadog'@'localhost' WITH MAX_USER_CONNECTIONS 5;
 GRANT PROCESS ON *.* TO 'datadog'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'datadog'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'datadog'@'localhost'
