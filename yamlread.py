4f8eec0173cadd89a3333fd152ef329ac8202a6c
szaporta <sarah.zaporta@datadoghq.com>
2017-12-19 09:48:36 -0500

    Update yamlread.py

--- yamlread.py
+++ yamlread.py
@@ -1,12 +1,12 @@
 yaml_list = []
 
-with open('mysql.yaml', 'r') as file_read:
+with open('/etc/dd-agent/conf.d/mysql.yaml', 'r') as file_read:
     for line in file_read:
         yaml_list.append(line)
         
 print(yaml_list)
 
-with open('mysql.yaml', 'w') as file_write:
+with open('/etc/dd-agent/conf.d/mysql.yaml', 'w') as file_write:
     for yaml_line in yaml_list:
         if '    # user: my_username' in yaml_line:
             file_write.write('    user: datadog\n')
