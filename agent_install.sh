78dfa8b9cbdf977132449ef409f93328b2de2719
szaporta <sarah.zaporta@datadoghq.com>
2017-12-19 14:41:58 -0500

    Update agent_install.sh

--- agent_install.sh
+++ agent_install.sh
@@ -1,7 +1,7 @@
 # NOTE: Change API KEY!
 
 # Set up dd-agent on Ubuntu 16.04
-DD_API_KEY=d5ed33bc1830f93767bbc1a16056ca95 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
+DD_API_KEY=API_KEY_GOES_HERE bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
 
 # Set up MySQL integration - no root pw needed
 mysql -u root mysql < dd-user_mysql_setup.sql
