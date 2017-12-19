f30958aac19a6a1b3927430a41cdc84ba558fbdc
szaporta <sarah.zaporta@datadoghq.com>
2017-12-19 11:47:27 -0500

    Update agent_install.sh

--- agent_install.sh
+++ agent_install.sh
@@ -4,7 +4,7 @@
 DD_API_KEY=d5ed33bc1830f93767bbc1a16056ca95 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
 
 # Set up MySQL integration
-mysql -u root -plimber mysql < dd-user_mysql_setup.sql
+mysql -u root -pdemodog mysql < dd-user_mysql_setup.sql
 
 # Restart Agent 
 sudo /etc/init.d/datadog-agent restart
